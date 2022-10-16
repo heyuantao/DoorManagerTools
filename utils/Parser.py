3c921853e4c866c60a679d69f66fc1914386ebcd
heyuantao <he_yuan_tao@163.com>
2022-10-16 09:36:09 +0800

    test

--- utils/Parser.py
+++ utils/Parser.py
@@ -41,14 +41,21 @@ class DoorManagerRecordParser:
         time_stamp = recordDict['passTimestamp']
         current_time_zone = pytz.timezone("Asia/Shanghai")
         the_time = datetime.datetime.fromtimestamp(time_stamp/1e3, tz=current_time_zone)
-        return  the_time 
+        return  the_time
+
+    @classmethod
+    def uuid(cls, recordDict):
+        time_stamp = recordDict['passTimestamp']
+        device_uuid = recordDict['deviceUuid']
+        record_unique_id = "{}{}".format(device_uuid,time_stamp)
+        return record_unique_id
 
 if __name__=="__main__":
     for line in call_back_str_array:
         decode_line = line.decode("UTF-8")
         dict_item = json.loads(decode_line)
-        print(dict_item)
-        print("发生开门事件在{}".format(DoorManagerRecordParser.time(dict_item)))
+        #print(dict_item)
+        print("发生开门事件在{},记录编号为{}".format(DoorManagerRecordParser.time(dict_item),DoorManagerRecordParser.uuid(dict_item)))
         if DoorManagerRecordParser.isUser(dict_item):
             print("教师 {} 打开了 {}".format(DoorManagerRecordParser.name(dict_item),DoorManagerRecordParser.location(dict_item)))
         if DoorManagerRecordParser.isGuest(dict_item):
--- views/DoorManagerToolsView.py
+++ views/DoorManagerToolsView.py
@@ -1,6 +1,7 @@
 #-*- coding=utf-8 -*-
 from flask import request, jsonify, current_app, stream_with_context, Response
 #from flask_api import status
+from utils.Parser import DoorManagerRecordParser
 import time
 #from werkzeug.urls import url_quote
 import urllib.parse
@@ -23,6 +24,14 @@ def version_view(request):
 
 def dooropen_view(request):
     data_str = request.data.decode("UTF-8")
-    data_dict = json.loads(data_str)
-    print(data_dict)
+    dict_item = json.loads(data_str)
+    # print(dict_item)
+    print("发生开门事件在{},记录编号为{}".format(DoorManagerRecordParser.time(dict_item), DoorManagerRecordParser.uuid(dict_item)))
+    if DoorManagerRecordParser.isUser(dict_item):
+        print("教师 {} 打开了 {}".format(DoorManagerRecordParser.name(dict_item), DoorManagerRecordParser.location(dict_item)))
+    if DoorManagerRecordParser.isGuest(dict_item):
+        print("{} 设置的访客 {} 打开了 {}".format(DoorManagerRecordParser.visitedName(dict_item),
+                                          DoorManagerRecordParser.name(dict_item),
+                                          DoorManagerRecordParser.location(dict_item)))
+
     return jsonify({'status':"ok"})
