9db387c230f21cae2d25928d265c4e3ce70909f5
heyuantao <he_yuan_tao@163.com>
2022-10-16 09:27:24 +0800

    s

--- utils/Parser.py
+++ utils/Parser.py
@@ -2,6 +2,8 @@
 
 from data.MegviiDoorManagerCallBackExampleData import call_back_str_array
 import json
+import datetime
+import pytz
 
 class DoorManagerRecordParser:
 
@@ -30,10 +32,23 @@ class DoorManagerRecordParser:
     def location(cls,recordDict):
         return recordDict['deviceName']
 
+    @classmethod
+    def timestamp(cls,recordDict):
+        return recordDict['passTimestamp']  #passTimestamp #passTime
+
+    @classmethod
+    def time(cls, recordDict):
+        time_stamp = recordDict['passTimestamp']
+        current_time_zone = pytz.timezone("Asia/Shanghai")
+        the_time = datetime.datetime.fromtimestamp(time_stamp/1e3, tz=current_time_zone)
+        return  the_time 
+
 if __name__=="__main__":
     for line in call_back_str_array:
         decode_line = line.decode("UTF-8")
         dict_item = json.loads(decode_line)
+        print(dict_item)
+        print("发生开门事件在{}".format(DoorManagerRecordParser.time(dict_item)))
         if DoorManagerRecordParser.isUser(dict_item):
             print("教师 {} 打开了 {}".format(DoorManagerRecordParser.name(dict_item),DoorManagerRecordParser.location(dict_item)))
         if DoorManagerRecordParser.isGuest(dict_item):
